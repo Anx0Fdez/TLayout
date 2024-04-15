525743f111ee7dff9a8835a32805fa61b05ce25e
Anx0Fdz <145323737+Anx0Fdez@users.noreply.github.com>
2024-04-15 11:53:34 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    private static String operation = "";
-    private static double firstNumber = 0;
-    private static double secondNumber = 0;
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Calculadora");
+        AtomicBoolean running = new AtomicBoolean(false);
+        AtomicInteger currentProgress = new AtomicInteger(0);
+
+        JButton playButton = new JButton("Play");
+        JButton pauseButton = new JButton("Pause");
+        JButton stopButton = new JButton("Stop");
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.X_AXIS));
+        controlPanel.add(playButton);
+        controlPanel.add(pauseButton);
+        controlPanel.add(stopButton);
+
+        JPanel keypadPanel = new JPanel(new GridLayout(3, 3));
+        for (int i = 1; i <= 9; i++) {
+            keypadPanel.add(new JButton(String.valueOf(i)));
+        }
+
+        JProgressBar progressBar = new JProgressBar();
+        progressBar.setMinimum(0);
+        progressBar.setMaximum(100);
+
+        playButton.addActionListener(e -> {
+            running.set(true);
+            new Thread(new ProgressRunnable(progressBar, running, currentProgress)).start();
+        });
+
+        pauseButton.addActionListener(e -> running.set(false));
+
+        stopButton.addActionListener(e -> {
+            running.set(false);
+            currentProgress.set(0);
+            progressBar.setValue(0);
+        });
+
+        JFrame frame = new JFrame("Media Control Panel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-
-        JTextField textField = new JTextField();
-        frame.add(textField, BorderLayout.NORTH);
-
-        JPanel numberPanel = new JPanel(new GridLayout(4, 4));
-        for (int i = 0; i <= 9; i++) {
-            JButton button = new JButton(String.valueOf(i));
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    textField.setText(textField.getText() + button.getText());
-                }
-            });
-            numberPanel.add(button);
-        }
-
-        String[] operations = {"+", "-"};
-        for (String op : operations) {
-            JButton button = new JButton(op);
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textField.getText());
-                    operation = op;
-                    textField.setText("");
-                }
-            });
-            numberPanel.add(button);
-        }
-
-        JButton equalsButton = new JButton("=");
-        equalsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                secondNumber = Double.parseDouble(textField.getText());
-                switch (operation) {
-                    case "+":
-                        textField.setText(String.valueOf(firstNumber + secondNumber));
-                        break;
-                    case "-":
-                        textField.setText(String.valueOf(firstNumber - secondNumber));
-                        break;
-                }
-            }
-        });
-        numberPanel.add(equalsButton);
-
-        frame.add(numberPanel, BorderLayout.CENTER);
-
-        JPanel operationPanel = new JPanel();
-        operationPanel.setLayout(new BoxLayout(operationPanel, BoxLayout.Y_AXIS));
-        JButton clearButton = new JButton("Limpiar");
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
-                firstNumber = 0;
-                secondNumber = 0;
-                operation = "";
-            }
-        });
-        operationPanel.add(clearButton);
-        frame.add(operationPanel, BorderLayout.EAST);
-
+        frame.add(progressBar, BorderLayout.NORTH);
+        frame.add(keypadPanel, BorderLayout.CENTER);
+        frame.add(controlPanel, BorderLayout.SOUTH);
         frame.pack();
         frame.setVisible(true);
     }
